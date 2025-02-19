b5a0536d07b1c6557dff12e5ab80be4120e27908
Rahul Suthar <90858008+Rahulcs27@users.noreply.github.com>
2025-02-19 16:01:16 +0530

    update

---
+++ scripts/script.js
@@ -0,0 +1,39 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+  window.addEventListener("scroll", function () {
+    const navbar = document.querySelector("nav");
+    if (window.scrollY > 50) {
+      navbar.classList.add("nav-scrolled");
+    } else {
+      navbar.classList.remove("nav-scrolled");
+    }
+  });
+
+  window.onload = function () {
+    window.scrollTo(0, 0); // Start at the top of the page
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const text = "Passionate Full-Stack Developer crafting stunning, interactive web experiences";
+  const typingElement = document.getElementById("typing-effect");
+  let index = 0;
+
+  function typeText() {
+    if (index < text.length) {
+      typingElement.innerHTML = text.substring(0, index + 1); // Update text
+      index++;
+      setTimeout(typeText, 50); // Adjust speed here (lower = faster)
+    }
+  }
+
+  // Start typing after a small delay to ensure smooth rendering
+  setTimeout(typeText, 500);
+});
+
+  
+  
