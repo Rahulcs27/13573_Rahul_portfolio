2f37ab27651204a531128d89d7ff61f7a47712b0
Rahul Suthar <90858008+Rahulcs27@users.noreply.github.com>
2025-02-19 13:24:46 +0530

    update

--- script.js
+++ script.js
@@ -5,26 +5,6 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-  // window.addEventListener("scroll", function () {
-  //   const navbar = document.querySelector("nav");
-  //   if (window.scrollY > 50) {
-  //     navbar.classList.add("nav-scrolled");
-  //   } else {
-  //     navbar.classList.remove("nav-scrolled");
-  //   }
-  // });
-  // window.addEventListener("scroll", function () {
-  //   const navbar = document.querySelector("nav");
-  //   const menuLinks = document.querySelector(".menu-links");
-
-  //   if (window.scrollY > 50) {
-  //     navbar.classList.add("nav-scrolled");
-  //     if (menuLinks) menuLinks.classList.add("nav-scrolled"); // Apply to mobile menu
-  //   } else {
-  //     navbar.classList.remove("nav-scrolled");
-  //     if (menuLinks) menuLinks.classList.remove("nav-scrolled"); // Remove from mobile menu
-  //   }
-  // });
   window.addEventListener("scroll", function () {
     const navbar = document.querySelector("nav");
     if (window.scrollY > 50) {
